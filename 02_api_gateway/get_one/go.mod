db5d70352feb2f2c6253f43e73a8e4519fc5c231
Nguyen Hiep <nguyen.tph128@gmail.com>
2024-11-28 10:07:10 +0700

    feat: add get one books api

---
+++ 02_api_gateway/get_one/go.mod
@@ -0,0 +1,5 @@
+module hello-lambda
+
+go 1.23.3
+
+require github.com/aws/aws-lambda-go v1.47.0
---
+++ 02_api_gateway/get_one/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Books struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Author string `json:"author"`
+}
+
+func getOne(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	books := []Books{
+		{Id: 1, Name: "NodeJS", Author: "NodeJS"},
+		{Id: 2, Name: "Golang", Author: "Golang"},
+		{Id: 3, Name: "Python", Author: "Hiep"},
+		{Id: 4, Name: "Java", Author: "James Gosling"},
+	}
+	
+	id, err := strconv.Atoi(req.PathParameters["id"])
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       req.Body + " checking",
+		}, nil
+	}
+
+	res, err := json.Marshal(books[id-1])
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       err.Error(),
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(res),
+	}, nil
+}
+
+func main() {
+	lambda.Start(getOne)
+}
